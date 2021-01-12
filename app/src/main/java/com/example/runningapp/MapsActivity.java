d8138c03fc234b5bab4a23a54c57245028e70d30
CoconutFresh <GlennBersabe@gmail.com>
2021-01-12 11:26:34 -0800

    Revamped GPS system
    - Switched from Google to android GPS service
    - Added a cautionary permission request if when launching map tracker.

--- app/src/main/java/com/example/runningapp/MapsActivity.java
+++ app/src/main/java/com/example/runningapp/MapsActivity.java
@@ -1,34 +1,41 @@
 package com.example.runningapp;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
 import android.util.Log;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.Cap;
+import com.google.android.gms.maps.model.JointType;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.android.gms.maps.model.RoundCap;
+import com.google.android.gms.maps.model.SquareCap;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
-    FusedLocationProviderClient fusedLocationProviderClient;
-    LocationCallback locationCallback;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
+    final int ZOOM_LEVEL = 18;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,7 +54,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
 
-        /*// Default if location is not set on
+        /*// Default if location is not set to on
         LatLng manila = new LatLng(15, 121);
         mMap.addMarker(new MarkerOptions().position(manila).title("Marker in Manila"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(manila));*/
@@ -59,38 +66,63 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         //TODO: Make Polyline look nicer
         //Creates settings for the runner's polyline route
         PolylineOptions runRouteSettings = new PolylineOptions();
-        runRouteSettings.clickable(false).color(Color.CYAN);
+        runRouteSettings.clickable(false)
+                .width(10)
+                .startCap(new RoundCap())
+                .jointType(JointType.ROUND)
+                .color(Color.CYAN);
 
-        Polyline runRoute = mMap.addPolyline(runRouteSettings);
 
         /*//Testing Polyline
         mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(15, 121)));
         mMap.animateCamera(CameraUpdateFactory.zoomTo(20));*/
 
-        requestLocation(runRoute, runRouteSettings);
+        getLocation(runRouteSettings);
+        //requestLocation(runRoute, runRouteSettings);
     }
 
-    private void requestLocation(Polyline runRoute, PolylineOptions runRouteTrack) {
-        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        locationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                super.onLocationResult(locationResult);
-                Log.d("mylog", "Lat is: " + locationResult.getLastLocation().getLatitude() + ", "
-                        + "Lng is: " + locationResult.getLastLocation().getLongitude());
+    private void getLocation(PolylineOptions runRouteTrack) {
 
-                LatLng curLoc = new LatLng(locationResult.getLastLocation().getLatitude(), locationResult.getLastLocation().getLongitude());
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Log.d("trackLog", "Lat is: " + location.getLatitude() + ", "
+                        + "Lng is: " + location.getLongitude());
+
+                LatLng curLoc = new LatLng(location.getLatitude(), location.getLongitude());
                 runRouteTrack.add(curLoc);
-                Polyline runRoute = mMap.addPolyline(runRouteTrack);
+                mMap.addPolyline(runRouteTrack);
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(curLoc));
-                mMap.animateCamera(CameraUpdateFactory.zoomTo(20));
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(ZOOM_LEVEL));
             }
         };
 
-        LocationRequest locationRequest = new LocationRequest();
-        locationRequest.setInterval(1000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        //Checks for permissions
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[] {
+                        Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET
+                }, 10);
+                return;
+            }
 
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startRun();
+                }
+                return;
+            default:
+                return;
+        }
     }
 }
