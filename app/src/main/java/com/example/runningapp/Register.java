454a8b8d98d251c18fe3bdbf44c542c373382e42
Josh Mejia <jmejia7951@gmail.com>
2021-01-13 13:39:30 -0800

    Connected to firebase db and created working registration form

--- app/src/main/java/com/example/runningapp/Register.java
+++ app/src/main/java/com/example/runningapp/Register.java
@@ -1,14 +1,152 @@
  package com.example.runningapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Register extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+ public class Register extends AppCompatActivity implements View.OnClickListener {
+    private TextView banner, registerUser;
+    private EditText fNameField, lNameField, birthdayField, usernameField, emailField, passwordField;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        banner = (TextView) findViewById(R.id.register_banner);
+        banner.setOnClickListener(this);
+
+        registerUser = (Button) findViewById(R.id.rg_button);
+        registerUser.setOnClickListener(this);
+
+        fNameField = (EditText) findViewById(R.id.user_firstname);
+        lNameField = (EditText) findViewById(R.id.user_lastname);
+        birthdayField = (EditText) findViewById(R.id.birthday_date);
+        usernameField = (EditText) findViewById(R.id.rg_username);
+        emailField = (EditText) findViewById(R.id.rg_email);
+        passwordField = (EditText) findViewById(R.id.rg_password);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
     }
-}
+
+     @Override
+     public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.banner:
+                startActivity(new Intent(this, Register.class));
+                break;
+            case R.id.rg_button:
+                registerUser();
+                break;
+        }
+     }
+
+     private void registerUser() {
+        String fName = fNameField.getText().toString().trim();
+        String lName = lNameField.getText().toString().trim();
+        String birthday = birthdayField.getText().toString().trim();
+        String username = usernameField.getText().toString().trim();
+        String email = emailField.getText().toString().trim();
+        String password = passwordField.getText().toString().trim();
+
+        if(fName.isEmpty()) {
+            fNameField.setError("First Name is required!");
+            fNameField.requestFocus();
+            return;
+        }
+
+        if(lName.isEmpty()) {
+            lNameField.setError("Last Name is required!");
+            lNameField.requestFocus();
+            return;
+        }
+
+        if(birthday.isEmpty()) {
+            birthdayField.setError("Date of Birth is required!");
+            birthdayField.requestFocus();
+            return;
+        }
+
+        if(username.isEmpty()) {
+            usernameField.setError("Username is required!");
+            usernameField.requestFocus();
+            return;
+        }
+
+        if(email.isEmpty()) {
+            emailField.setError("Email is required!");
+            emailField.requestFocus();
+            return;
+        }
+        // email pattern validation e.g. jollibee.must@die.com
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailField.setError("Please provide a valid email!");
+            emailField.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()) {
+            passwordField.setError("Password is required!");
+            passwordField.requestFocus();
+            return;
+        }
+        // need a minimum of 6 characters because firebase says to git fukd;
+        if(password.length() < 6) {
+            passwordField.setError("Minimum password length is 6 characters!");
+            passwordField.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            User user = new User(fName, lName, birthday, username, email);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                    .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if(task.isSuccessful()) {
+                                        Toast.makeText(Register.this, "Account has been created successfully!", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                    else {
+                                        Toast.makeText(Register.this, "Registration failed, please try again", Toast.LENGTH_LONG).show();
+                                        progressBar.setVisibility(View.GONE);
+                                    }
+                                }
+                            });
+                        }
+                        else {
+                            Toast.makeText(Register.this, "Registration failed.", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
+     }
+ }
---
+++ app/src/main/java/com/example/runningapp/User.java
@@ -0,0 +1,16 @@
+package com.example.runningapp;
+
+public class User {
+
+    public String fName, lName, birthday, username, email;
+
+    public User() {}
+
+    public User(String fName, String lName, String birthday, String username, String email) {
+        this.fName = fName;
+        this.lName = lName;
+        this.birthday = birthday;
+        this.username = username;
+        this.email = email;
+    }
+}
