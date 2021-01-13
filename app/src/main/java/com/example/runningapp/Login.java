724ae824a1cbdcb80c7fab1e5b69ec46059e5454
Josh Mejia <jmejia7951@gmail.com>
2021-01-13 15:18:01 -0800

    Created login method

--- app/src/main/java/com/example/runningapp/Login.java
+++ app/src/main/java/com/example/runningapp/Login.java
@@ -1,21 +1,31 @@
 package com.example.runningapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText username;
-    private EditText password;
+    private EditText emailField, passwordField;
+    private TextView register,  forgotPassword;
     private Button login;
-    private TextView register;
-    private TextView forgotPassword;
+
+    private FirebaseAuth mAuth;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +38,15 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
         forgotPassword = (TextView) findViewById(R.id.forgot_password);
         forgotPassword.setOnClickListener(this);
 
-        username = findViewById(R.id.lg_username);
-        password = findViewById(R.id.lg_password);
+        emailField = (EditText) findViewById(R.id.lg_email);
+        passwordField = (EditText) findViewById(R.id.lg_password);
 
-        login = findViewById(R.id.lg_button);
+        login = (Button) findViewById(R.id.lg_button);
         login.setOnClickListener(this);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -45,20 +59,62 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
                 //TODO: Create password link
                 break;
             case R.id.lg_button:
-                //TODO: Create a proper login service
-                validate(username.getText().toString(), password.getText().toString()); //temporary
+                //validate(username.getText().toString(), password.getText().toString()); //temporary
+                login();
                 break;
         }
     }
 
-    //TODO: Create actual login process
-    //Garbage method for testing. Will delete later.
-    private void validate(String un, String pw) {
+    private void login(){
+        String email = emailField.getText().toString().trim();
+        String password = passwordField.getText().toString().trim();
+
+        if(email.isEmpty()) {
+            emailField.setError("Email is required!");
+            emailField.requestFocus();
+            return;
+        }
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            emailField.setError("Email is invalid!");
+            emailField.requestFocus();
+            return;
+        }
+
+        if(password.isEmpty()) {
+            passwordField.setError("Password is required!");
+            passwordField.requestFocus();
+            return;
+        }
+
+        if(password.length() < 6) {
+            passwordField.setError("Minimum password length is 6 characters!");
+            passwordField.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    // redirect to profile home
+                    startActivity(new Intent(Login.this, HomePage.class));
+                }
+                else{
+                    Toast.makeText(Login.this, "Login Failed! Please check your credentials.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    //Garbage method for testing. Will delete later. Left it in case shit goes wrong with Firebase
+    /*private void validate(String un, String pw) {
         if(un.equals("test") && pw.equals("test")) {
 
             //Switches over control of the app to the next activity
             startActivity(new Intent(this, HomePage.class));
             finish(); //Makes it so that the user can't use the back button to return to this page.
         }
-    }
+    }*/
 }
