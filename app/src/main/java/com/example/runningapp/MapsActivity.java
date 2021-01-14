2b1cffbea4707dac1831ebf6f552738a996ea022
CoconutFresh <GlennBersabe@gmail.com>
2021-01-14 15:18:18 -0800

    Maps Activity Updated
    +Added Marker for current location
         -Note: Gonna look for a way for it to use android location library instead
    +Improved logic for tracking
    +Added smoother movement
    
    Note: Fragments for maps activity are not working yet. Will work on it tomorrow.

--- app/src/main/java/com/example/runningapp/MapsActivity.java
+++ app/src/main/java/com/example/runningapp/MapsActivity.java
@@ -2,11 +2,15 @@ package com.example.runningapp;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
+import android.graphics.Camera;
 import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
@@ -17,11 +21,14 @@ import android.os.Looper;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.Cap;
 import com.google.android.gms.maps.model.JointType;
@@ -32,75 +39,40 @@ import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.maps.model.RoundCap;
 import com.google.android.gms.maps.model.SquareCap;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener, LocationListener {
     private GoogleMap mMap;
     LocationManager locationManager;
-    LocationListener locationListener;
+    PolylineOptions runRouteOptions;
+    CameraPosition initialCamera;
 
-    Button button_start_run;
+    Fragment initializeRunFragment, runningFragment;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
 
-    final int ZOOM_LEVEL = 18;
+    Button bt_startRun, bt_stopRun, bt_startRun_test;
+
+    boolean isRunning = false, initialState = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        button_start_run = findViewById(R.id.button_start_run);
-        button_start_run.setOnClickListener(this);
-
-    }
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-
-
-    }
-
-    private void startRun() {
-
-        //TODO: Make Polyline look nicer
-        //Creates settings for the runner's polyline route
-        PolylineOptions runRouteSettings = new PolylineOptions();
-        runRouteSettings.clickable(false)
-                .width(10)
+        //For line graphing
+        runRouteOptions = new PolylineOptions();
+        runRouteOptions.clickable(false)
+                .width(20)
                 .startCap(new RoundCap())
                 .jointType(JointType.ROUND)
                 .color(Color.CYAN);
 
-
-        /*//Testing Polyline
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(15, 121)));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(20));*/
-
-        getLocation(runRouteSettings);
-        //requestLocation(runRoute, runRouteSettings);
-    }
-
-    private void getLocation(PolylineOptions runRouteTrack) {
-
+        //For finding current location of device
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                Log.d("trackLog", "Lat is: " + location.getLatitude() + ", "
-                        + "Lng is: " + location.getLongitude());
-
-                LatLng curLoc = new LatLng(location.getLatitude(), location.getLongitude());
-                runRouteTrack.add(curLoc);
-                mMap.addPolyline(runRouteTrack);
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(curLoc));
-                mMap.animateCamera(CameraUpdateFactory.zoomTo(ZOOM_LEVEL));
-            }
-        };
-
         //Checks for permissions
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -112,8 +84,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
 
         }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 0, this);
 
+        //test
+        bt_startRun_test = findViewById(R.id.bt_startRun_test);
+        bt_startRun_test.setOnClickListener(this);
+
+        /*bt_startRun = findViewById(R.id.bt_startRun);
+        bt_startRun.setOnClickListener(this);
+        bt_stopRun = findViewById(R.id.bt_stopRun);
+        bt_stopRun.setOnClickListener(this);*/
+
+        /*initializeRunFragment = new InitializeRunFragment();
+        runningFragment = new RunningFragment();*/
+
+        /*fragmentManager = getSupportFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fl_saveme, initializeRunFragment);
+        fragmentTransaction.commit();*/
+
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.setMyLocationEnabled(true);
+        //mMap.animateCamera(CameraUpdateFactory.zoomTo(ZOOM_LEVEL));
+    }
+
+    private void startRun(LatLng location) {
+
+        //TODO: Make Polyline look nicer
+
+        Log.d("polylineDebug", "This is being called");
+        runRouteOptions.add(location);
+        mMap.addPolyline(runRouteOptions);
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(location));
     }
 
     @Override
@@ -121,7 +129,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         switch (requestCode) {
             case 10:
                 if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    startRun();
+                    //startRun();
                 }
                 return;
             default:
@@ -132,11 +140,47 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.button_start_run:
-                startRun();
+            case R.id.bt_startRun_test:
+                /*fragmentTransaction = fragmentManager.beginTransaction();
+                //fragmentTransaction.replace(R.id.fl_saveme, runningFragment);
+                fragmentTransaction.commit();*/
+                isRunning = true;
+                //startRun();
                 break;
+            case R.id.bt_stopRun:
+                /*fragmentTransaction = fragmentManager.beginTransaction();
+                //fragmentTransaction.replace(R.id.fl_saveme, initializeRunFragment);
+                fragmentTransaction.commit();
+                isRunning = false;*/
             default:
                 break;
+        }
+    }
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+        Log.d("trackLog", "Lat is: " + location.getLatitude() + ", "
+                + "Lng is: " + location.getLongitude());
+
+        LatLng curLoc = new LatLng(location.getLatitude(), location.getLongitude());
+
+        if (isRunning) {
+            startRun(curLoc);
+            Toast.makeText(this, "Starting run", Toast.LENGTH_SHORT).show();
+        }
+        else if (!isRunning && initialState) {
+            initialCamera = new CameraPosition.Builder()
+                    .target(curLoc)
+                    .zoom(18)
+                    .bearing(0)
+                    .tilt(0)
+                    .build();
+
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(initialCamera));
+            initialState = false;
+        } else if (!isRunning && !initialState) {
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(curLoc));
         }
     }
 }
