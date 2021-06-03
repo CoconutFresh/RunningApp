7d522645b649719d75545d9b3de5e190d0a11182
Josh Mejia <jmejia7951@gmail.com>
2021-06-03 16:44:51 -0700

    time to fix this database

--- app/src/main/java/com/example/runningapp/User.java
+++ app/src/main/java/com/example/runningapp/User.java
@@ -3,14 +3,16 @@ package com.example.runningapp;
 public class User {
 
     public String fName, lName, birthday, username, email;
-
+    public String [] runData;
+    
     public User() {}
 
-    public User(String fName, String lName, String birthday, String username, String email) {
+    public User(String fName, String lName, String birthday, String username, String email, String [] runData) {
         this.fName = fName;
         this.lName = lName;
         this.birthday = birthday;
         this.username = username;
         this.email = email;
+        this.runData = runData;
     }
 }
